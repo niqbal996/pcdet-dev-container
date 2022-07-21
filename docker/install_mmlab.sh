ee5e011b186ce1bfd67f1952682f46c14b5ae3c3
Naeem <naeemiqbal996@gmail.com>
2022-07-21 09:10:48 +0200

    Added more submodules

--- docker/install_mmlab.sh
+++ docker/install_mmlab.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-cd /workspace/pcdet-dev/thirdparty/OpenPCDet
-python setup.py develop
 mim install mmcv-full
 mim install mmdet
+cd /workspace/pcdet-dev/thirdparty/OpenPCDet
+python setup.py develop
 cd /workspace/pcdet-dev/thirdparty/mmdetection3d
 python setup.py develop
 cd /workspace/pcdet-dev/thirdparty/mmdeploy
